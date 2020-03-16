4ff472977d86ef2b462456e42443a78e278675fa
Pascal Young <pascalyoung03@gmail.com>
2020-03-16 16:01:53 -0700

    fix

--- js/globals.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  'use strict';
-
-  window.USERNAME = 'padlocks'; // github username
-  window.REPO = 'blog'; // github repository
-  window.TOKEN = '2d6dcbf526f86644e2eabfe109fd562c0b0b2370'; // github app access token
-}());
